4ef0c51397435c51c9f5aff460c78a03b0992240
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-06 16:14:05 +0000

    Label generated from baseColor

--- src/components/picker.tsx
+++ src/components/picker.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Color from 'color';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { changeBaseColor } from 'redux/paletteSlice';
@@ -25,8 +26,14 @@ const Well = styled.div<WellProps>`
   box-shadow: inset 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
-const Details = styled.p`
+const Label = styled.ul`
   font-size: ${p => p.theme.sizing.small};
+  margin: ${p => p.theme.sizing.large};
+  margin-left: 0;
+  width: 5em;
+  line-height: ${p => p.theme.fonts.larger};
+  font-size: ${p => p.theme.fonts.small};
+  list-style: none;
 `;
 
 export const Picker = () => {
@@ -34,10 +41,22 @@ export const Picker = () => {
   const dispatch = useDispatch();
   const handleClick = () => dispatch(changeBaseColor('#ffc900'));
 
+  const baseHSL = Color(baseColor)
+    .hsl()
+    .round()
+    .string();
+
+  const channels = ['H: ', 'S: ', 'L: '];
+  const values = baseHSL.slice(4, -1).split(',');
+
   return (
     <Container>
       <Well bg={baseColor} onClick={() => handleClick()} />
-      <Details />
+      <Label>
+        {channels.map((channel, i) => (
+          <li>{`${channel}${values[i]}`}</li>
+        ))}
+      </Label>
     </Container>
   );
 };
