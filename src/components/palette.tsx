ba3e9fafd3786be4db72bb4e21fc77fa7ca1917f
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-06 10:41:24 +0000

    Retrieve value from state

--- src/components/palette.tsx
+++ src/components/palette.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/rootReducer';
+
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -8,18 +11,20 @@ const Container = styled.div`
   height: 150px;
 `;
 
-const Swatch = styled.div`
+const Swatch = styled.div<{ bg: string }>`
   width: 50px;
   height: 50px;
   border-radius: 4px;
-  background: ${p => p.theme.colors.accent};
+  background: ${p => p.bg};
   box-shadow: inset 0px 1px 2px 0px rgba(0, 0, 0, 0.2);
 `;
 
 export const Palette = () => {
+  const baseColor = useSelector((state: RootState) => state.palette.baseColor);
+
   return (
     <Container>
-      <Swatch />
+      <Swatch bg={baseColor} />
     </Container>
   );
 };
