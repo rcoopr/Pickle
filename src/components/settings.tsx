f3ff2aef9163e4ec192bf083bec4b17d9db7bb56
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-11 23:56:54 +0000

    General cleanup

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import Color from 'color';
 
-import { swatchesHSL } from 'utils/swatchColors';
 import { useSelector } from 'react-redux';
 import { RootState } from 'redux/rootReducer';
 import { Graph } from 'components/graph';
 
 const Container = styled.section`
-/* background: ${p => p.theme.colors.tertiary}; */
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const Settings = () => {
-  const baseColor = useSelector((state: RootState) => state.palette.baseColor);
-  const data = swatchesHSL(Color(baseColor));
-
-  const hue = {
+const channels = {
+  hue: {
     name: 'Hue',
     channel: 0,
-  };
-  const sat = {
+  },
+  sat: {
     name: 'Saturation',
     channel: 1,
-  };
-  const lit = {
+  },
+  light: {
     name: 'Lightness',
     channel: 2,
-  };
+  },
+};
+
+export const Settings = () => {
+  const data = useSelector((state: RootState) => state.palette.swatches);
 
   return (
     <Container>
-      <Graph xAxis={lit} yAxis={sat} data={data} width={300} height={200} />
-      <Graph xAxis={lit} yAxis={hue} data={data} width={300} height={200} />
+      <Graph xAxis={channels.light} yAxis={channels.sat} data={data} width={300} height={200} />
+      <Graph xAxis={channels.light} yAxis={channels.hue} data={data} width={300} height={200} />
     </Container>
   );
 };
