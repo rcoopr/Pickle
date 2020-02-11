6c19803cc4e30a3c8fd3ea73c31bcdd1094d14ba
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-11 23:55:42 +0000

    Added functions to alter saturation/lightness

--- src/utils/swatchColors.ts
+++ src/utils/swatchColors.ts
@@ -1,21 +1,46 @@
 import Color from 'color';
 
-export const swatchesCSS = (baseColor: Color) => {
-  const emptyArray = [...Array(9).fill(null)];
-  const swatches = emptyArray.map((_, i) => baseColor.lighten(0.8 - i * 0.2).string());
+const clamp = (value: number, min: number, max: number) => Math.min(Math.max(value, min), max);
 
-  return swatches;
+const linearSpread = (numItems: number) =>
+  Array(numItems)
+    .fill(0)
+    .map((_, i) => (i + 1) / (numItems + 1));
+
+export const halfSine = (amplitude: number, verticalShift: number, period = 2, phaseShift = 0) => (
+  x: number,
+) => {
+  return amplitude * Math.sin(((2 * Math.PI) / period) * (x + phaseShift)) + verticalShift;
 };
 
-export const swatchesHSL = (baseColor: Color) => {
-  const emptyArray = [...Array(9).fill(null)];
-  const swatches = emptyArray.map((_, i) =>
-    baseColor
-      .lighten(0.8 - i * 0.2)
-      .round()
-      .hsl()
-      .array(),
-  );
+export const hslStringToArray = (hslString: string) =>
+  hslString
+    .match(/[0-9]+/g)! // Catch each color channel
+    .map(val => parseInt(val, 10)); // Return an int
 
+export const hslArrayToString = (hslArray: number[][]) =>
+  hslArray.map(hsl => `hsl(${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%)`);
+
+export const swatchesCSS = (swatches: number[][]) =>
+  swatches.map(hslArray => Color.hsl(hslArray).hex());
+
+export const deriveSwatches = (
+  baseColor: string,
+  params: number, // [-50, 50] - central point has +/-50% saturation than end points
+) => {
+  const baseColorHSL = hslStringToArray(baseColor);
+
+  const amplitude = params;
+  const verticalShift = baseColorHSL[1];
+  const partialHalfSine = halfSine(amplitude, verticalShift);
+
+  const lightness = linearSpread(9);
+  const saturation = lightness.map(val => clamp(partialHalfSine(val), 0, 100));
+
+  const swatches = lightness.map((lightValue, i) => [
+    baseColorHSL[0],
+    saturation[i],
+    100 * lightValue,
+  ]);
   return swatches;
 };
