03be8b900d937b0cd7b4cfdbb5961fd6012ae45c
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-16 23:52:34 +0000

    Added unit tests for hsl conversion functions

--- src/utils/hslConvert.ts
+++ src/utils/hslConvert.ts
@@ -2,39 +2,47 @@ import Color from 'color';
 
 /**
  * For conversion between human-readable string format to arrays of color channels
- * @param {string} hslString of format 'hsl(h, s%, l%)',
+ * @param {string} hslString of format 'hsl(H, S%, L%)',
  * h represents hue from 0-359,
  * s represents hue from 0-100,
  * l represents hue from 0-100,
  *
- * @return {number[]} [h, s, l]
+ * @return {number[]} [H, S, L]
  */
 export const hslStringToArray = (hslString: string) =>
   hslString.match(/[0-9]+/g)!.map(val => parseInt(val, 10));
 
 /**
  * For conversion between array of color channels to human-readable string format
- * @param  {number[]} hslArray of format [h, s, l],
+ * @param  {number[]} hslArray of format [H, S, L],
  * h represents hue from 0-359,
  * s represents hue from 0-100,
  * l represents hue from 0-100,
  *
- * @return {string} 'hsl(h, s, l)'
+ * @return {string} 'hsl(H, S, L)'
  */
 export const hslArrayToString = (hslArray: number[]) =>
   `hsl(${hslArray[0]}, ${hslArray[1]}%, ${hslArray[2]}%)`;
 
 /**
  * Used to export the palette in a 'CSS-usable' format
- * @param  {string[]} strings of format 'hsl(h, s%, l%)'
+ * @param  {string[]} strings of format 'hsl(H, S%, L%)'
  */
 export const formatStringsToCopy = (strings: string[]) =>
   strings.map((string, i) => `${(i + 1) * 100}: '${string}',`);
 
 /**
+ * Used to export the palette in HSL format, each channel rounded to nearest int
+ * @param  {number[][]} swatches derived data from user-selected color
+ * swatches array contains hslArrays - [H, S, L]
+ */
+export const swatchesHSL = (swatches: number[][]) =>
+  swatches.map(swatch => hslArrayToString(swatch.map(channel => parseFloat(channel.toFixed(0)))));
+
+/**
  * Converts from HSL into Hex format
  * @param  {number[][]} swatches derived data from user-selected color
- * swatches array contains hslArrays - [h, s, l]
+ * swatches array contains hslArrays - [H, S, L]
  */
 export const swatchesHex = (swatches: number[][]) =>
   swatches.map(swatch =>
@@ -44,17 +52,9 @@ export const swatchesHex = (swatches: number[][]) =>
   );
 
 /**
- * Used to export the palette in HSL format, each channel rounded to nearest int
- * @param  {number[][]} swatches derived data from user-selected color
- * swatches array contains hslArrays - [h, s, l]
- */
-export const swatchesHSL = (swatches: number[][]) =>
-  swatches.map(swatch => hslArrayToString(swatch.map(channel => parseFloat(channel.toFixed(0)))));
-
-/**
  * Used to export the palette in RGB format, each channel rounded to nearest int
  * @param  {number[][]} swatches derived data from user-selected color
- * swatches array contains hslArrays - [h, s, l]
+ * swatches array contains hslArrays - [H, S, L]
  */
 export const swatchesRGB = (swatches: number[][]) =>
   swatches.map(swatch =>
