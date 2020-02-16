c963b4b705ff6c21cec7990473f3944e2591ae45
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-16 17:22:08 +0000

    Comments added to utils. Maybe too many comments

--- src/utils/hslConvert.ts
+++ src/utils/hslConvert.ts
@@ -1,16 +1,43 @@
 import Color from 'color';
 
+/**
+ * For conversion between human-readable string format to arrays of color channels
+ * @param {string} hslString of format 'hsl(h, s%, l%)',
+ * h represents hue from 0-359,
+ * s represents hue from 0-100,
+ * l represents hue from 0-100,
+ *
+ * @return {number[]} [h, s, l]
+ */
 export const hslStringToArray = (hslString: string) =>
   hslString
     .match(/[0-9]+/g)! // Catch each color channel
     .map(val => parseInt(val, 10)); // Return an int
 
+/**
+ * For conversion between array of color channels to human-readable string format
+ * @param  {number[]} hslArray of format [h, s, l],
+ * h represents hue from 0-359,
+ * s represents hue from 0-100,
+ * l represents hue from 0-100,
+ *
+ * @return {string} 'hsl(h, s, l)'
+ */
 export const hslArrayToString = (hslArray: number[]) =>
   `hsl(${hslArray[0]}, ${hslArray[1]}%, ${hslArray[2]}%)`;
 
+/**
+ * Used to export the palette in a 'CSS-usable' format
+ * @param  {string[]} strings of format 'hsl(h, s%, l%)'
+ */
 export const formatStringsToCopy = (strings: string[]) =>
-  strings.map((string, i) => `${(i + 1) * 100}: ${string};`);
+  strings.map((string, i) => `'${(i + 1) * 100}': '${string}',`);
 
+/**
+ * Converts from HSL into Hex format
+ * @param  {number[][]} swatches derived data from user-selected color
+ * swatches array contains hslArrays - [h, s, l]
+ */
 export const swatchesHex = (swatches: number[][]) =>
   swatches.map(swatch =>
     Color.hsl(swatch)
@@ -18,9 +45,19 @@ export const swatchesHex = (swatches: number[][]) =>
       .toString(),
   );
 
+/**
+ * Used to export the palette in HSL format, each channel rounded to nearest int
+ * @param  {number[][]} swatches derived data from user-selected color
+ * swatches array contains hslArrays - [h, s, l]
+ */
 export const swatchesHSL = (swatches: number[][]) =>
-  swatches.map(swatch => hslArrayToString(swatch.map(channel => parseFloat(channel.toFixed(1)))));
+  swatches.map(swatch => hslArrayToString(swatch.map(channel => parseFloat(channel.toFixed(0)))));
 
+/**
+ * Used to export the palette in RGB format, each channel rounded to nearest int
+ * @param  {number[][]} swatches derived data from user-selected color
+ * swatches array contains hslArrays - [h, s, l]
+ */
 export const swatchesRGB = (swatches: number[][]) =>
   swatches.map(swatch =>
     Color.hsl(swatch)
--- src/utils/swatchColors.ts
+++ src/utils/swatchColors.ts
@@ -1,39 +1,68 @@
 import { hslStringToArray } from 'utils/hslConvert';
 
+// Returns value no smaller than min, no larger than max
 const clamp = (value: number, min: number, max: number) => Math.min(Math.max(value, min), max);
 
+// Return numItems as #values spread evenly from min to max
 const linearSpread = (numItems: number, min = 0, max = 1) =>
   Array(numItems)
     .fill(0)
     .map((_, i) => {
+      // Spread values evenly from 0 to 1
       const valBetween0and1 = (i + 1) / (numItems + 1);
+      // Scale by the output range and increase all values by minimum output value
       return valBetween0and1 * (max - min) + min;
     });
 
+// Internal function - Maps linear input values (lightness) to variable output (saturation and hue)
+// Curried to reduce extrraneous function creations. Each time the user-selected color changes,
+// the partial function is called once for each tone
 export const halfSine = (amplitude: number, verticalShift: number, period = 2, phaseShift = 0) => (
   x: number,
 ) => {
+  // General form of sine wave is dependent on these 4 variables;
+  // Amplitude: maximum height offset from origin,
+  // Period: length of repeating segment,
+  // Phase shift: negative horizontal offset from origin,
+  // Vertical shift: vertical offset from origin
   return amplitude * Math.sin(((2 * Math.PI) / period) * (x + phaseShift)) + verticalShift;
 };
 
+/**
+ * @param  {string} baseColor user-selected color
+ * @param  {number} saturationDelta difference in saturation from the most-saturated tone to the least
+ * @param  {number} hueDelta difference in hue from the first tone to the last
+ */
 export const deriveSwatches = (baseColor: string, saturationDelta: number, hueDelta: number) => {
   const baseColorHSL = hslStringToArray(baseColor);
 
+  // Lightness as a value from [0, 1]; Forms a base unit value to scale saturation from
   const baseLightnessFraction = baseColorHSL[2] / 100;
-  const lightnessSpace = Math.min(1 - baseLightnessFraction, baseLightnessFraction);
+  // The smaller of the 2 distances from [0, baselightnessFraction] and [baselightnessFraction, 1]
+  const lightnessSpace = Math.min(baseLightnessFraction, 1 - baseLightnessFraction);
+  // Spread values across the space. This prevents values exceeding the edges of the graph
   const lightness = linearSpread(
     9,
     baseLightnessFraction - lightnessSpace,
     baseLightnessFraction + lightnessSpace,
   );
+  // Spread hue according to the hue delta chosen by the user
   const hue = linearSpread(9, baseColorHSL[0] - hueDelta / 2, baseColorHSL[0] + hueDelta / 2);
 
+  // Height of curve is equal to the saturation delta chosen by the user
   const amplitude = saturationDelta;
+  // Shifts the curve up the graph as saturation increases. Subtract the amplitude so as saturation
+  // delta changes, the ends of the curve changes, not the middle
   const verticalShift = baseColorHSL[1] - amplitude;
+  // 0 values can cause NaN values to be sent to the svg circle elements
+  // The 'width' of the curve is kept from exceeding the edges of the graph
   const period = Math.max(lightnessSpace * 2 * 2, 0.001);
+  // As the user picks lighter colors, the curve shifts appropriately
   const phaseShift = -(baseLightnessFraction - lightnessSpace);
+  // Without partial the same function is recreated numTimes (9 by default)
   const partialHalfSine = halfSine(amplitude, verticalShift, period, phaseShift);
 
+  // return [vals] between [0, 100] for generated saturation
   const saturation = lightness.map(val => clamp(partialHalfSine(val), 0, 100));
 
   const swatches = lightness.map((lightValue, i) => [hue[i], saturation[i], 100 * lightValue]);
