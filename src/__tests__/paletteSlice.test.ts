56fe099a70b423dcf24768a8f188e827366004d3
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-17 11:00:48 +0000

    More coverage added

--- src/__tests__/paletteSlice.test.ts
+++ src/__tests__/paletteSlice.test.ts
@@ -21,9 +21,46 @@ describe('palette slice', () => {
     });
 
     it('should update baseColor on dispatch', () => {
-      const data = 'hsl(1, 63%, 62%)';
+      const data = 'hsl(0, 60%, 60%)';
       const nextState = reducer(initialState, setBaseColor(data));
-      expect(selectBaseColor(nextState)).toBe('hsl(1, 63%, 62%)');
+      expect(selectBaseColor(nextState)).toBe(data);
+    });
+
+    it('should update saturationDelta on dispatch', () => {
+      const data = -20;
+      const nextState = reducer(initialState, setSaturationDelta(data));
+      expect(selectSaturationDelta(nextState)).toBe(data);
+    });
+
+    it('should update hueDelta on dispatch', () => {
+      const data = 20;
+      const nextState = reducer(initialState, setHueDelta(data));
+      expect(selectHueDelta(nextState)).toBe(data);
+    });
+
+    it('should update swatches on dispatch', () => {
+      const data = Array(9).fill([0, 10, 20]);
+      const nextState = reducer(initialState, setSwatches(data));
+      expect(selectSwatches(nextState)).toBe(data);
+    });
+
+    it('should not affect hue/sat delta when using setBaseColor', () => {
+      const data = 'hsl(0, 60%, 60%)';
+      const nextState = reducer(initialState, setBaseColor(data));
+      expect(selectSaturationDelta(nextState)).toBe(initialState.saturationDelta);
+      expect(selectHueDelta(nextState)).toBe(initialState.hueDelta);
+    });
+
+    it('should not affect hue delta when using setSaturationDelta', () => {
+      const data = 30;
+      const nextState = reducer(initialState, setSaturationDelta(data));
+      expect(selectHueDelta(nextState)).toBe(initialState.hueDelta);
+    });
+
+    it('should not affect sat delta when using setHueDelta', () => {
+      const data = 30;
+      const nextState = reducer(initialState, setHueDelta(data));
+      expect(selectSaturationDelta(nextState)).toBe(initialState.saturationDelta);
     });
 
     it('should update swatches when baseColor changes', () => {
