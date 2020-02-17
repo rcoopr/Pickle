7b467fb2efc37766c98b67910cbf90c2d1935163
Froskk <37559715+Froskk@users.noreply.github.com>
2020-02-17 00:56:28 +0000

    Improved unit testing on redux slice (WIP)

--- src/__tests__/paletteSlice.test.ts
+++ src/__tests__/paletteSlice.test.ts
@@ -1,25 +1,36 @@
-/* eslint-disable jest/no-disabled-tests */
-/* eslint-disable jest/no-test-prefixes */
-import palette, { setBaseColor } from 'redux/paletteSlice';
+import { Action, PayloadAction, ThunkAction } from '@reduxjs/toolkit';
+import reducer, {
+  initialState,
+  setBaseColor,
+  setSaturationDelta,
+  setHueDelta,
+  setSwatches,
+  updateStateIfDiff,
+  selectBaseColor,
+  selectSaturationDelta,
+  selectHueDelta,
+  selectSwatches,
+} from 'redux/paletteSlice';
 
-describe('palette reducer', () => {
-  xit('should handle initial state', () => {
-    expect(palette(undefined, {})).toEqual({ baseColor: '#1fc6ea' });
-  });
+describe('palette slice', () => {
+  describe('reducer, actions and selectors', () => {
+    it('should return the initial state on first run', () => {
+      const nextState = initialState;
+      const result = reducer(undefined, { type: undefined });
+      expect(result).toBe(nextState);
+    });
 
-  xit('should handle changing base color', () => {
-    expect(
-      palette(
-        { baseColor: '#1fc6ea' },
-        {
-          type: setBaseColor.type,
-          payload: '#f7cf7d',
-        },
-      ),
-    ).toEqual({ baseColor: '#f7cf7d' });
-  });
+    it('should update baseColor on dispatch', () => {
+      const data = 'hsl(1, 63%, 62%)';
+      const nextState = reducer(initialState, setBaseColor(data));
+      expect(selectBaseColor(nextState)).toBe('hsl(1, 63%, 62%)');
+    });
 
-  xit('should not accept numbers as base color', () => {
-    expect(palette({}, { type: setBaseColor.type, payload: 1 })).toThrowError();
+    it('should update swatches when baseColor changes', () => {
+      const data = 'hsl(1, 63%, 62%)';
+      const nextState = reducer(initialState, updateStateIfDiff(data));
+      const nextSwatches = selectSwatches(nextState);
+      expect(nextSwatches[4]).toBe('hsl(1, 63%, 62%)');
+    });
   });
 });
